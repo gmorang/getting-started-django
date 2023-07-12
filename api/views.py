973351a7f57a5333c63f9bfe6cfb5b258fc60659
Gabriel Morandim <augustomorandim@gmail.com>
2023-07-12 15:55:52 -0300

    feat: implementing CRUD functions

---
+++ api/views.py
@@ -0,0 +1,80 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import NoteSerializer
+from .models import Note
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return an array of notes',
+        },
+        {
+            'endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return a single note object',
+        },
+        {
+            'endpoint': '/notes/create',
+            'method': 'POST',
+            'body': {'body': ''},
+            'description': 'Create new note with data sent in post request body',
+        }
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+
+    note = Note.objects.create(
+        body=data['body']
+    )
+
+    serializer = NoteSerializer(note, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    note = Note.objects.get(id=pk)
+
+    serializer = NoteSerializer(note, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+
+    note.delete()
+
+    return Response('Note was deleted!')
